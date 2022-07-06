18c3048c1b25e7cda31c133b3058d8948c2e1880
xuejiu <1764343065@qq.com>
2022-07-06 09:15:02 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection service = new ServiceCollection();
+            service.AddLogging(logBuilder=> 
+            {
+                logBuilder.AddConsole();
+                logBuilder.SetMinimumLevel(LogLevel.Trace);//error
+            });
+            service.AddScoped<Test>();
+            using (var sp = service.BuildServiceProvider()) 
+            {
+                var oneTest= sp.GetRequiredService<Test>();
+                oneTest.OneTest();
+            }
+
+
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingLearning
+{
+    public class Test
+    {
+        private readonly ILogger<Test> _logger;
+        public Test(ILogger<Test> logger)
+        {
+            _logger = logger;
+        }
+        public void OneTest() 
+        {
+            _logger.LogDebug("开始连接数据库");
+            _logger.LogDebug("连接数据库成功");
+            _logger.LogWarning("查找数据失败,重试");
+            _logger.LogWarning("查找数据失败,重试第二次");
+            _logger.LogError("查找数据失败");
+            try
+            {
+                File.ReadAllText(@"D:\desktop\数据库使用1.txt");
+                _logger.LogError("读取文件成功");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,"读取文件失败");
+            }
+
+        }
+
+
+    }
+}
